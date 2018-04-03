4db42484fe9d6177fefe05b961e51a602c157c20
Eric Seidler <eric.seidler@asynchrony.com>
2018-04-02 21:03:44 -0500

    feature: add some styling to DeckListItem

--- components/DeckListItem.js
+++ components/DeckListItem.js
@@ -1,11 +1,36 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, StyleSheet, Dimensions } from 'react-native'
+import { gray } from '../utils/colors'
 
 export default function DeckListItem({ item }) {
   return (
-    <View>
-      <Text>{item.title}</Text>
-      <Text>{item.questions.length} card{item.questions.length === 1 ? '' : 's'}</Text>
+    <View style={[styles.container, { width: Dimensions.get('window').width }]}>
+      <Text style={styles.title}>{item.title}</Text>
+      <Text
+        style={styles.numQuestions}
+      >
+        {item.questions.length} card{item.questions.length === 1 ? '' : 's'}
+      </Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: 150,
+    flexDirection: 'column',
+    alignItems: 'center',
+    paddingTop: 15,
+    paddingBottom: 15,
+    borderBottomWidth: 1,
+  },
+  title: {
+    fontSize: 24,
+    flex: 1,
+  },
+  numQuestions: {
+    color: gray,
+    fontSize: 16,
+  },
+})
