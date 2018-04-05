8591e18d2aa8843f5b07bb4806f699a02cc9247b
Eric Seidler <eric.seidler@asynchrony.com>
2018-04-05 08:01:20 -0500

    feature: create placeholder for new deck view

---
+++ components/NewDeck.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export default function NewDeck() {
+  return (
+    <View>
+      <Text>Here is a view for a new deck</Text>
+    </View>
+  )
+}
