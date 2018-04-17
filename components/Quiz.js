df3489a9474862364003f38c8bbbfe7a0900bcdb
Eric Seidler <eric.seidler@asynchrony.com>
2018-04-16 20:01:02 -0500

    feature: retrieve Deck when Quiz renders

--- components/Quiz.js
+++ components/Quiz.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
+import { getDeck } from '../utils/helpers'
 import { white } from '../utils/colors'
 
 export default class Quiz extends Component {
   static navigationOptions = () => ({ title: 'Quiz' })
+  constructor() {
+    super()
+
+    this.state = {
+      deck: null
+    }
+  }
+  componentDidMount() {
+    const { deckName } = this.props.navigation.state.params
+    getDeck(deckName).then((deck) => {
+      this.setState({ deck })
+    })
+  }
   render() {
+    if (!this.state.deck) {
+      return null
+    }
+    const { title } = this.state.deck
     return (
-      <View style={styles.container}><Text>Here is the Quiz View!</Text></View>
+      <View style={styles.container}><Text>{`Here is the Quiz View for ${title}!`}</Text></View>
     )
   }
 }
