ba8f75f08d48f8e59a7d29ab8b11bf79569c8bdf
Eric Seidler <eric.seidler@asynchrony.com>
2018-04-16 20:28:40 -0500

    feature: update Quiz to cycle through question titles, maintain correct answer count

--- components/Quiz.js
+++ components/Quiz.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, Button } from 'react-native'
 import { getDeck } from '../utils/helpers'
-import { white } from '../utils/colors'
+import { white, red, green } from '../utils/colors'
 
 export default class Quiz extends Component {
   static navigationOptions = () => ({ title: 'Quiz' })
@@ -9,25 +9,52 @@ export default class Quiz extends Component {
     super()
 
     this.state = {
-      deck: null
+      deck: null,
+      currentQuestionIndex: null,
+      numCorrectQuestions: 0
     }
   }
   componentDidMount() {
     const { deckName } = this.props.navigation.state.params
     getDeck(deckName).then((deck) => {
-      this.setState({ deck })
+      this.setState({ deck, currentQuestionIndex: 0 })
     })
+  }
+  moveToNextQuestion = (knewCorrectAnswer) => {
+    this.setState(prevState => ({
+      numCorrectQuestions: prevState.numCorrectQuestions + (knewCorrectAnswer ? 1 : 0),
+      currentQuestionIndex: prevState.currentQuestionIndex + 1
+    }))
   }
   render() {
     if (!this.state.deck) {
       return null
     }
-    const { questions } = this.state.deck
+    const { deck, deck: { questions }, currentQuestionIndex } = this.state
+    const currentQuestion = deck.questions[currentQuestionIndex]
     return (
       <View style={styles.container}>
         {questions.length === 0
-          ? <Text style={styles.atLeastOneQuestion}>You must have at least one question to take the quiz!</Text>
-          : <Text>Start questions here</Text>
+          ? (
+            <Text style={styles.atLeastOneQuestion}>
+              You must have at least one question to take the quiz!
+            </Text>
+          )
+          : (
+            <React.Fragment>
+              <Text style={styles.questionTitle}>{currentQuestion.question}</Text>
+              <Button
+                title="Correct"
+                color={green}
+                onPress={() => this.moveToNextQuestion(true)}
+              />
+              <Button
+                title="Incorrect"
+                color={red}
+                onPress={() => this.moveToNextQuestion(false)}
+              />
+            </React.Fragment>
+          )
         }
       </View>
     )
@@ -43,5 +70,8 @@ const styles = StyleSheet.create({
   },
   atLeastOneQuestion: {
     fontSize: 28
+  },
+  questionTitle: {
+    fontSize: 36
   }
 })
